f988ff97f1cd8fd10b796cc22a7fc8dc7f764a50
Vaelzan <vaelzan@evilcult.net>
2022-03-30 09:16:21 +1100

    Hide a few more items from REI (when KubeJS is back...).

--- kubejs/client_scripts/rei_blacklist.js
+++ kubejs/client_scripts/rei_blacklist.js
@@ -7,6 +7,20 @@ onEvent('rei.hide.items', event => {
     'minecraft:light',
     'minecraft:structure_block',
     'minecraft:structure_void',
-    'chiselsandbits:block_bit'
+    'alexsmobs:tab_icon',
+    'chiselsandbits:block_bit',
+    'citadel:debug',
+    'citadel:citadel_book',
+    'citadel:effect_item',
+    'citadel:fancy_item',
+    'titanium:block_test',
+    'titanium:block_twenty_four_test',
+    'titanium:block_asset_test',
+    'titanium:machine_test',
+    'titanium:creative_generator',
+    'immersiveengineering:fake_icon_birthday',
+    'immersiveengineering:fake_icon_lucky',
+    'immersiveengineering:fake_icon_drillbreak',
+    'immersiveengineering:fake_icon_ravenholm'
   ]);
 });
