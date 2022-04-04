3440c22f0eb1c36067e05f92d3af83029c8360fa
Vaelzan <vaelzan@evilcult.net>
2022-04-05 00:36:21 +1000

    Add getting started message + link to DIscord on first start.

--- kubejs/server_scripts/first_start.js
+++ kubejs/server_scripts/first_start.js
@@ -3,6 +3,6 @@ onEvent('player.logged_in', function (event) {
     event.player.stages.add('first_start')
 
     event.player.tell(Text.translate('valhelsia.first_start_message'))
-    event.player.tell(Text.translate('valhelsia.getting_started_guide').blue().underlined().click('https://wiki.valhelsia.net/modpacks/valhelsia-enhanced-vanilla/getting-started-guide'))
+    event.player.tell(Text.translate('valhelsia.discord').blue().underlined().click('https://discord.com/invite/rdfQuaM'))
   }
 })
