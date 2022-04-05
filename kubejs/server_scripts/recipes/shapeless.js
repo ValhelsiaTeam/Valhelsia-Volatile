c33b5ed8bfe85325187230831bd9229c00778596
Vaelzan <vaelzan@evilcult.net>
2022-04-05 20:22:00 +1000

    Add 3x cattail to string recipe.
    Future-proofed by adding BYG cattail to forge:crops/cattail tag and using that.

--- kubejs/server_scripts/recipes/shapeless.js
+++ kubejs/server_scripts/recipes/shapeless.js
@@ -1,4 +1,7 @@
 // priority: 10
+// Valhelsia: Volatile
+// Shapeless Crafting Recipes
 
 onEvent('recipes', event => {
-})
+  event.shapeless('minecraft:string', ['#forge:crops/cattail', '#forge:crops/cattail', '#forge:crops/cattail']);
+});
--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -1,7 +1,15 @@
 // priority: 110
 // Valhelsia: Volatile
 // Item Tags
+// These are mostly for recipe inputunification, but sometimes have other uses.
 
 onEvent('tags.items', event => {
+
+  event.get('forge:crops').add([
+    'alexsmobs:banana',
+    'byg:cattail_sprout',
+  ]);
+
+  event.add('forge:crops/cattail', 'byg:cattail_sprout');
 
 });
