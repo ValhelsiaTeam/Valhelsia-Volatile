e9a46a286ba201ddb893ff2c24474f2fce327b9d
Vaelzan <vaelzan@evilcult.net>
2022-04-05 20:01:57 +1000

    Replace canvas with wool in Bundle recipe (temporary) + remove QuartZ Elevator recipe..

--- kubejs/server_scripts/recipes/shaped.js
+++ kubejs/server_scripts/recipes/shaped.js
@@ -1,15 +1,16 @@
 // priority: 10
+// Shaped Crafting Recipes
 
 onEvent('recipes', function (event) {
+  // Convenience Functions:
+  let shaped2x2 = (output, input) => {event.shaped(output, ['AA', 'AA'], {A: input})};
+  let shaped3x3 = (output, input) => {event.shaped(output, ['AAA', 'AAA', 'AAA'], {A: input})};
+  let donut = (output, input) => {event.shaped(output, ['AAA', 'A A', 'AAA'], {A: input})};
+  let wrapped = (output, core, wrap) => {event.shaped(output, ['AAA', 'ABA', 'AAA'], {A: wrap, B: core})};
+  let barrel = (output, plank, slab) => {event.shaped(output, ['ABA', 'A A', 'ABA'], {A: plank, B: slab})};
 
   // Minecraft
-  event.shaped('minecraft:cobweb', [
-    'AAA',
-    'AAA',
-    'AAA'
-  ], {
-    A: 'minecraft:string'
-  })
+  shaped3x3('minecraft:cobweb', 'minecraft:string');
 
   event.shaped('minecraft:dispenser', [
     'AAA',
@@ -19,7 +20,7 @@ onEvent('recipes', function (event) {
     A: 'minecraft:cobblestone',
     B: 'minecraft:crossbow',
     C: 'minecraft:redstone'
-  })
+  });
 
   event.shaped('minecraft:dispenser', [
     'RS ',
@@ -29,14 +30,14 @@ onEvent('recipes', function (event) {
     R: 'minecraft:stick',
     S: 'minecraft:string',
     D: 'minecraft:dropper'
-  })
+  });
 
   event.shaped('minecraft:chainmail_helmet', [
     'AAA',
     'A A'
   ], {
     A: 'minecraft:chain'
-  })
+  });
 
   event.shaped('minecraft:chainmail_chestplate', [
     'A A',
@@ -44,7 +45,7 @@ onEvent('recipes', function (event) {
     'AAA'
   ], {
     A: 'minecraft:chain'
-  })
+  });
 
   event.shaped('minecraft:chainmail_leggings', [
     'AAA',
@@ -52,13 +53,14 @@ onEvent('recipes', function (event) {
     'A A'
   ], {
     A: 'minecraft:chain'
-  })
+  });
+
   event.shaped('minecraft:chainmail_boots', [
     'A A',
     'A A'
   ], {
     A: 'minecraft:chain'
-  })
+  });
   
   event.shaped('minecraft:bundle', [
     'SCS',
@@ -66,16 +68,7 @@ onEvent('recipes', function (event) {
     'CCC'
   ], {
     S: 'minecraft:string',
-    C: 'farmersdelight:canvas'
-  })
+    C: '#minecraft:wool'
+  });
 
-  // Quartz Elevator
-  event.shaped('quartzelv:quartz_elevator', [
-    'BBB',
-    'BAB',
-    'BBB'
-  ], {
-    A: 'architects_palette:ender_pearl_block',
-    B: 'minecraft:quartz'
-  })
-})
+});
