b31fb7ddb2e434d7282be52e3899a9f3c7b84589
Vaelzan <vaelzan@evilcult.net>
2022-04-05 23:02:35 +1000

    Add recipes to make mossy stone and mossy red rock bricks using moss blocks.

--- kubejs/server_scripts/recipes/shapeless.js
+++ kubejs/server_scripts/recipes/shapeless.js
@@ -3,5 +3,10 @@
 // Shapeless Crafting Recipes
 
 onEvent('recipes', event => {
+  // Minecraft
   event.shapeless('minecraft:string', ['#forge:crops/cattail', '#forge:crops/cattail', '#forge:crops/cattail']);
+
+  // BYG
+  event.shapeless('byg:mossy_stone', ['minecraft:stone', 'minecraft:moss_block']);
+  event.shapeless('byg:mossy_red_rock_bricks', ['byg:red_rock_bricks', 'minecraft:moss_block']);
 });
