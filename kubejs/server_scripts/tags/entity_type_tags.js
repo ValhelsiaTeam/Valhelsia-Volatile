81838269a25f9eb6a28f66b9f984d3177c04d7de
Vaelzan <vaelzan@evilcult.net>
2022-04-05 20:22:34 +1000

    Add Entity Type Tags script w/ Valhelsia Tweaks entries.

---
+++ kubejs/server_scripts/tags/entity_type_tags.js
@@ -0,0 +1,29 @@
+// priority: 110
+// Valhelsia: Volatile
+// Entity Type Tags
+
+onEvent('entity_type.tags', event => {
+
+  // Add Halloween Event Candy to Spoopy Mobs
+  event.get('valhelsia_tweaks:drops_candy').add([
+    'minecraft:zombie',
+    'minecraft:husk',
+    'minecraft:skeleton',
+    'minecraft:stray',
+    'minecraft:witch',
+    'minecraft:spider',
+    'minecraft:phantom',
+    'minecraft:cave_spider',
+    'quark:wraith',
+    'quark:forgotten',
+    'goblintraders:goblin_trader', // Arguably not spoopy enough, but I'm sure they horde candy.
+    'goblintraders:vein_goblin_trader',
+    'graveyard:acolyte',
+    'graveyard:ghoul',
+    'graveyard:nightmare',
+    'graveyard:reaper',
+    'graveyard:revenant',
+    'graveyard:skeleton_creeper',
+  ]);
+
+});
