6cf5c9d142afa159d4e8e8ecc1027a9496ef5d2e
Vaelzan <vaelzan@evilcult.net>
2022-04-05 20:47:09 +1000

    Add missing Sandstone block + item tags.
    Mostly useful for future crafting recipes (particularly crushing back into sand).

--- kubejs/server_scripts/tags/block_tags.js
+++ kubejs/server_scripts/tags/block_tags.js
@@ -3,5 +3,79 @@
 // Block Tags
 
 onEvent('tags.blocks', event => {
+  // Sandstones
+  event.get('forge:sandstone').add([
+    'byg:black_smooth_sandstone',
+    'byg:blue_smooth_sandstone',
+    'byg:pink_smooth_sandstone',
+    'byg:purple_smooth_sandstone',
+    'byg:white_smooth_sandstone',
+    'quark:sandstone_bricks',
+    'quark:red_sandstone_bricks',
+  ]);
 
+  event.get('forge:sandstone/black').add([
+    'byg:black_sandstone',
+    'byg:black_chiseled_sandstone',
+    'byg:black_cut_sandstone',
+    'byg:black_smooth_sandstone',
+  ]);
+
+  event.get('forge:sandstone/blue').add([
+    'byg:blue_sandstone',
+    'byg:blue_chiseled_sandstone',
+    'byg:blue_cut_sandstone',
+    'byg:blue_smooth_sandstone',
+  ]);
+
+  event.get('forge:sandstone/colorless').add([
+    'quark:sandstone_bricks',
+  ]);
+
+  event.get('forge:sandstone/pink').add([
+    'byg:pink_sandstone',
+    'byg:pink_chiseled_sandstone',
+    'byg:pink_cut_sandstone',
+    'byg:pink_smooth_sandstone',
+  ]);
+
+  event.get('forge:sandstone/purple').add([
+    'byg:purple_sandstone',
+    'byg:purple_chiseled_sandstone',
+    'byg:purple_cut_sandstone',
+    'byg:purple_smooth_sandstone',
+  ]);
+
+  event.get('forge:sandstone/red').add([
+    'quark:red_sandstone_bricks',
+  ]);
+
+  event.get('forge:sandstone/soul').add([
+    'quark:soul_sandstone',
+    'quark:soul_sandstone_bricks',
+    'quark:smooth_soul_sandstone',
+    'quark:cut_soul_sandstone',
+    'quark:chiseled_soul_sandstone',
+  ]);
+
+  event.get('forge:sandstone/soulless').add([
+    'forbidden_arcanus:soulless_sandstone',
+    'forbidden_arcanus:cut_soulless_sandstone',
+    'forbidden_arcanus:polished_soulless_sandstone',
+  ]);
+
+  event.get('forge:sandstone/windswept').add([
+    'byg:windswept_sandstone',
+    'byg:chiseled_windswept_sandstone',
+    'byg:cut_windswept_sandstone',
+    'byg:smooth_windswept_sandstone',
+    'byg:windswept_sandstone_pillar',
+  ]);
+
+  event.get('forge:sandstone/white').add([
+    'byg:white_sandstone',
+    'byg:white_chiseled_sandstone',
+    'byg:white_cut_sandstone',
+    'byg:white_smooth_sandstone',
+  ]);
 });
