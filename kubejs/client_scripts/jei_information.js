0e82db646579ccad0a0b0ed9d9c62580a44d6d51
Vaelzan <vaelzan@evilcult.net>
2022-04-21 18:00:28 +1000

    Create initial JEI scripts (mostly empty for now).

---
+++ kubejs/client_scripts/jei_information.js
@@ -0,0 +1,29 @@
+// priority: 1
+
+//   __   ___   _    _  _ ___ _    ___ ___   _   
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\  
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \ 
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\
+//     __   _____  _      _ _____ ___ _    ___     
+//     \ \ / / _ \| |    /_\_   _|_ _| |  | __|    
+//      \ V / (_) | |__ / _ \| |  | || |__| _|     
+//       \_/ \___/|____/_/ \_\_| |___|____|___|    
+//
+
+/**
+ * @file Custom JEI Information additions for Valhelsia: Volatile.
+ * 
+ * Used to provide additional information to item recipes + uses where appropriate to allow 
+ * players to learn how to obtain / use items without external sources (such as wikis).
+ * 
+ * Entries should use translatable text components with language entries added to
+ * assets/valhelsia/lang/en_us.json so that they can be translated via CrowdIn.
+ *  
+ */
+
+/**
+ * JEI Information Event Handler
+ */
+onEvent('jei.information', event => {
+  // TODO: JEI Information here.
+});
